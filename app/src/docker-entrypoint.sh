f32d724c74db0d10833aed3fa7f75861f536c957
Alin Voinea <alin@eaudeweb.ro>
2016-10-27 18:58:05 +0300

    Refs #77731 - Handle id_rsa key

--- app/src/docker-entrypoint.sh
+++ app/src/docker-entrypoint.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
+# Add github to known_hosts
 if [ ! -e /root/.ssh/known_hosts ]; then
+  mkdir -p /root/.ssh
   ssh-keyscan github.com > /root/.ssh/known_hosts
+  chmod 0700 /root/.ssh
 fi
 
+# SSH key
+if [ ! -e /root/.ssh/id_rsa ]; then
+  if [ -e /src/id_rsa ]; then
+    cp /src/id_rsa /root/.ssh/
+    chmod 0600 /root/.ssh/id_rsa
+  fi
+fi
+
+# Generate docs
 if [[ "$1" == "http"* ]] || [[ "$1" == "git@"* ]]; then
   exec python update_package.py "$@"
 fi
 
+# Anything else
 exec "$@"
